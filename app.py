84df93f783ea8411b99f941a1b8e33b18a345843
roshita <roshlogin@gmail.com>
2026-05-17 20:37:36 +0530

    added API

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import google.generativeai as genai
+
+# Gemini API Key
+genai.configure(api_key="AIzaSyDUL-ObUYbhy0NVDijEY086fyRW4oansT8")
+
+# Load Gemini model
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("index.html")
 
-if __name__=="__main__":
+    response = ""
+
+    if request.method == "POST":
+
+        user_input = request.form["question"]
+
+        ai_response = model.generate_content(user_input)
+
+        response = ai_response.text
+
+    return render_template("index.html", response=response)
+
+if __name__ == "__main__":
     app.run(debug=True)
