fc01f86c02e95dfec48538bd69069f4a7a8d8d3b
roshita <roshlogin@gmail.com>
2026-05-31 17:16:17 +0530

    Build AI study assistant UI and hide API key

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask, render_template, request
 from google import genai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.getenv("GEMINI_API_KEY")
 
 # Create Gemini client
-client = genai.Client(api_key="AIzaSyBHVyJsjoV-LI2QoHKKiER4Jx49DmxE1sk")
+client = genai.Client(api_key=api_key)
 
 app = Flask(__name__)
 
@@ -10,20 +15,39 @@ app = Flask(__name__)
 def home():
 
     response = ""
+    question = ""
 
     if request.method == "POST":
 
         user_input = request.form["question"]
+        question = user_input
 
-        # Send request to Gemini
-        ai_response = client.models.generate_content(
-            model="gemini-2.0-flash",
-            contents=user_input
-        )
+        try:
+            study_prompt = f"""
+You are an AI Study Assistant.
 
-        response = ai_response.text
+Your job:
+- Explain concepts clearly
+- Use simple student-friendly language
+- Give step-by-step answers
+- Use examples when helpful
+- If the student asks for homework help, explain the method first
 
-    return render_template("index.html", response=response)
+Student question:
+{user_input}
+"""
+
+            ai_response = client.models.generate_content(
+                model="gemini-2.5-flash",
+                contents=study_prompt
+            )
+
+            response = ai_response.text
+
+        except Exception as e:
+            response = "Sorry, something went wrong. Please try again after a few minutes."
+
+    return render_template("index.html", response=response,  question=question)
 
 if __name__ == "__main__":
     app.run(debug=True)
