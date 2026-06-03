221d92febf1d94910d881a27ee105020b0fb4aeb
roshita <roshlogin@gmail.com>
2026-06-03 23:55:22 +0530

    added type of response

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 from google import genai
 import os
 from dotenv import load_dotenv
@@ -6,23 +6,21 @@ from dotenv import load_dotenv
 load_dotenv()
 api_key = os.getenv("GEMINI_API_KEY")
 
-# Create Gemini client
 client = genai.Client(api_key=api_key)
 
 app = Flask(__name__)
+app.secret_key = "study-assistant-secret"
 
 @app.route("/", methods=["GET", "POST"])
 def home():
 
-    response = ""
-    question = ""
+    history = session.get("history", [])
 
     if request.method == "POST":
 
         user_input = request.form["question"]
         subject = request.form["subject"]
         answer_style = request.form["answer_style"]
-        question = user_input
 
         try:
             study_prompt = f"""
@@ -52,7 +50,16 @@ Student question:
         except Exception as e:
             response = "Sorry, something went wrong. Please try again after a few minutes."
 
-    return render_template("index.html", response=response,  question=question)
+        history.append({
+            "question": user_input,
+            "answer": response,
+            "subject": subject,
+            "answer_style": answer_style
+        })
+
+        session["history"] = history
+
+    return render_template("index.html", history=history)
 
 if __name__ == "__main__":
     app.run(debug=True)
